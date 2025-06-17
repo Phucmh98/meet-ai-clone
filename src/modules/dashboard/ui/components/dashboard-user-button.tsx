02808b46e1c40b3dce0602899690409d2ae11a64
Phuc <mhphuc.98@gmail.com>
2025-06-17 17:32:26 +0700

    new cmt

---
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -0,0 +1,33 @@
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { authClient } from "@/lib/auth-client";
+
+export const DashboardUserButton = () => {
+  const { data, isPending } = authClient.useSession();
+  if (isPending || !data?.user) {
+    return null;
+  }
+  console.log("User Data:", data);
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+        {data.user.image ? (
+          <Avatar>
+            <AvatarImage src={data.user.image} />
+          </Avatar>
+        ) : (
+          <GeneratedAvatar
+            seed={data.user.name}
+            variant="initials"
+            className="size-0 mr-3"
+          />
+        )}
+        
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
+};
